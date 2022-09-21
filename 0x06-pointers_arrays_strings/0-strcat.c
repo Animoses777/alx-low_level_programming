cca903967e3dd1526db147e82177bfb02f72cb1d
Animoses777 <animoses9@gmail.com>
2022-09-21 15:16:11 -0700

    All task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,18 +5,18 @@
 *the end of the string pointed to by @dest
 *@dest: String that will be appended
 *@src: String to be concatenated upon
-*@n: integer parameter to compare index to
+*
 * Return: returns pointer to @dest
 */
 
-char *_scrcat(char *dest, char *src, int n)
+char *_scrcat(char *dest, char *src,)
 {
 	int index = 0, dest_len = 0;
 	
 	while (dest[index++])
 		dest_len++;
 		
-	for (index = 0; src[index] && index < n; index++)
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 		
 	return (dest);
